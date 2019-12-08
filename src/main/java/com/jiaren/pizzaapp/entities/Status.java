f020b4055a75058f73d81dda443277afbb69e9c5
chenchuqiao <chuqiao.chan@gmail.com>
2019-12-08 17:05:09 +1100

    Creating "Order" and "OrderedPizza" entities, and updating JSONB data type with Hibernate related lib in build.gradle.

---
+++ src/main/java/com/jiaren/pizzaapp/entities/Status.java
@@ -0,0 +1,8 @@
+package com.jiaren.pizzaapp.entities;
+
+public enum Status {
+    CREATE,
+    PROCESS,
+    deliver,
+    COMPLETE
+}
---
+++ src/main/java/com/jiaren/pizzaapp/repositories/OrderedPizzaRepository.java
@@ -0,0 +1,4 @@
+package com.jiaren.pizzaapp.repositories;
+
+public interface OrderedPizzaRepository {
+}
