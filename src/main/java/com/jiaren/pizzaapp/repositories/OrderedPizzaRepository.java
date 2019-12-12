2fb2d375d806d699e1955306276b28384b1af13d
chenchuqiao <chuqiao.chan@gmail.com>
2019-12-12 17:04:07 +1100

    Code reformat with OrderedPizzaRepository.java

--- src/main/java/com/jiaren/pizzaapp/repositories/OrderedPizzaRepository.java
+++ src/main/java/com/jiaren/pizzaapp/repositories/OrderedPizzaRepository.java
@@ -2,11 +2,6 @@ package com.jiaren.pizzaapp.repositories;
 
 import com.jiaren.pizzaapp.entities.OrderedPizza;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
-import java.util.Map;
 
 public interface OrderedPizzaRepository extends JpaRepository<OrderedPizza, Long> {
 
